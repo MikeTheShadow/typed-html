e2b625ec375f9e9024022671aa9d43aa5a1ad551
MikeTheShadow <michaelpape11@gmail.com>
2024-03-17 02:03:51 -0400

    Added hx-trigger

--- macros/src/config.rs
+++ macros/src/config.rs
@@ -41,7 +41,7 @@ pub fn global_attrs(span: Span) -> StringyMap<Ident, TokenStream> {
         insert("hx_trigger","String");
         insert("hx_confirm","String");
         insert("hx_delete","String");
-        insert("hx-trigger","String");
+        insert("hx_trigger","String");
         // FIXME ARIA and XML attrs missing
     }
     attrs
